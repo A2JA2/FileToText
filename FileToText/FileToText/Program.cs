662ef94d74f9cc48f468b9c1e696d442839380f5
A2JA2 <55232721+A2JA2@users.noreply.github.com>
2020-02-03 10:57:34 +0000

    Add files via upload

---
+++ FileToText/FileToText/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileToText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string line = Console.ReadLine();
+
+            // set a variable to the My Document path.
+            string mydocpath =
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // write the string array to a new file name "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(mydocpath, "Data_Export.txt")))
+            {
+                //foreach (string line in lines)
+                outputFile.WriteLine(line);
+            }
+        }
+    }
+}
